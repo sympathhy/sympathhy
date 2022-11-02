10a4c1a5526fdfedbf1fbcb8576879913ed149ae
sympathhy <zhidelev1832@gmail.com>
2022-11-02 15:11:52 +0500

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+def text_lower(text):
+    return text.lower()
+
+def length_text(text):
+    return len(text)
+
+
+if __name__ == '__main__':
+    n = 'ПЕРВЫЙ РЕПОЗИТОРИЙ НА ГИТЕ'
+    print(text_lower(n))
+    print(length_text(n))
